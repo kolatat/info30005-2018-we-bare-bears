df5653b3ffca34be6c31bf2bdc56ba219fe9329f
Mei Chin <k.chin7@student.unimelb.edu.au>
2018-04-11 12:40:02 +1000

    Created basic Milestones and Leaderboard pages
    - Created basic Milestones and Leaderboard pages
    - Added routing on indexRoutes.ts

--- routes/indexRoutes.ts
+++ routes/indexRoutes.ts
@@ -2,19 +2,29 @@ import * as express from 'express';
 
 const router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
     res.render('coming_soon');
 });
 
-/* GET world page. */
+/* GET World page. */
 router.get('/world/', function(req, res, next) {
     res.render('world');
 });
 
-/* GET faw page. */
+/* GET FAQ page. */
 router.get('/faq/', function(req, res, next) {
     res.render('faq');
+});
+
+/* GET Leaderboard page. */
+router.get('/leaderboard/', function(req, res, next) {
+    res.render('leaderboard');
+});
+
+/* GET Milestones page. */
+router.get('/milestones/', function(req, res, next) {
+    res.render('milestones');
 });
 
 export default router;
