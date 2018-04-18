0c8615d4ed9fb7e408050a6562118cfd85006896
Kolatat Thangkasemvathana <kolatat.t@gmail.com>
2018-04-18 16:18:49 +1000

    Moved API to /api/, added 404 catchall

--- app.ts
+++ app.ts
@@ -6,21 +6,27 @@ import indexRouter from './routes/indexRoutes';
 const Log = debug('wbb:main');
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+const apiRouter = express.Router();
 
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
-
 app.use('/', indexRouter);
-app.use('/users', userRouter);
+app.use('/api', apiRouter);
 
-app.use((req, res) => {
+apiRouter.use('/users', userRouter);
+apiRouter.use((req, res) => {
     res.status(404).send(JSON.stringify({
-        error: new Error('Not found')
+        error: new Error('Not found'),
+        message: "Not found"
     }));
 })
+
+app.use((req,res)=>{
+    res.render('error/404', {
+        url: req.originalUrl
+    });
+});
 
 app.listen(port, function () {
     Log(`express listening on port ${port}`);
