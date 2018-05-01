f13680662acb176a0ef5fe97cb21e6f645f695f4
Mei Chin <k.chin7@student.unimelb.edu.au>
2018-05-01 18:12:23 +1000

    Dummy Function

--- public/js/new_play.js
+++ public/js/new_play.js
@@ -1,4 +1,5 @@
 
+
 /* Toggle the PopUp Window that displays the quizzes */
 function toggleQuizWindow(){
     //Set a variable to contain the DOM element of the overlay
@@ -25,23 +26,23 @@ function toggleMessageWindow(obj){
     var overlay = document.getElementById("overlay_message");
     //Set a variable to contain the DOM element of the popup
     var popup = document.getElementById("popup_message");
-    // Toggle visibility of overlay and popup
 
 
     var obj_class = obj.className;
 
+
+    // Toggle visibility of overlay and popup
     if(overlay.style.display === "none" || overlay.style.display === ""){
         overlay.style.display = "block";
         popup.style.display = "block";
-        //Edit part of the text in popup based on ID of function caller
-//
 
+        // Insert message depending on the class of element (correct/wrong answer)
+        //Edit part of the text in popup based on ID of function caller
         if(obj_class === "correct"){
             document.getElementById("msg_insert").innerHTML = "Correct Answer!"
         } else {
             document.getElementById("msg_insert").innerHTML = "Wrong Answer!"
         }
-
     } else {
         overlay.style.display = "none";
         popup.style.display = "none";
@@ -50,17 +51,39 @@ function toggleMessageWindow(obj){
 
 
 
+//
+function getRandomQuestion(){
+    var ques = {
+
+        question: "Dummy question #1",
+        ques_img: "",   // to store question image
+        options: ["hi", "ko", "laaa", "help", "pick mee!"],
+        correct_ans: "pick mee!",
+        options_img: ""     // to store images for each option
+    };
+
+    return ques;
+}
+
+//
+function getRandomVideoLink(){
+    var video = {
+        url: "https://www.youtube.com/embed/_LXlxSZI_K8"
+    };
+
+    return video;
+}
 
 
+
+// Testing
 $(document).ready(function() {
 
     var $quiz_container = $('#quiz_container');
-    var test_mult_param = {options: ["hi", "ko", "laaa", "help", "pick mee!"], correct_ans: "pick mee!"};
     var test_blank_param = {fill_array_objects: [{type: "fill", value: "Rendering this "}, {type: "blank", value: "first answer"}, {type: "fill", value: "Insert stuff here"}, {type: "blank", value:"second answer"}, {type:"fill", value:"yay"}, {type:"blank", value:"this comes after yay"}]};
-    var test_vid_param = {url: "https://www.youtube.com/embed/_LXlxSZI_K8"};
 
     $('#multiple').click(function () {
-        $.get('play_mult', test_mult_param)
+        $.get('play_mult', getRandomQuestion())
            .done(function(data){
                $quiz_container.html(data);
            });
@@ -74,7 +97,7 @@ $(document).ready(function() {
     });
 
     $('#vid').click(function() {
-        $.get('play_video', test_vid_param)
+        $.get('play_video', getRandomVideoLink())
             .done(function(data){
             $quiz_container.html(data);
         });
