8df9441addfac408d4dd6984175ca535be696158
Mei Chin <slchin_56@hotmail.com>
2018-05-12 11:48:55 +1000

    Rerouting index to login page

--- routes/indexRoutes.ts
+++ routes/indexRoutes.ts
@@ -1,97 +1,9 @@
 import * as express from 'express'
 
 const router = express.Router();
-//
-// /* GET Home page. */
-// router.get('/', function(req, res, next) {
-//     res.render('coming_soon');
-// });
-//
-// /* GET New Play page. */
-// router.get('/new/play/', function(req, res, next) {
-//     res.render('play/');
-// });
-//
-// router.get('/new/play_mult', function(req, res, next){
-//    res.render('play/mult_choice', req.query);
-// });
-//
-// router.get('/new/play_blanks', function(req, res, next){
-//     res.render('play/blanks', req.query);
-// });
-//
-// router.get('/new/play_video', function(req, res, next){
-//     res.render('play/video', req.query);
-// });
-// /* GET New Create page. */
-// router.get('/new/create/', function(req, res, next) {
-//     res.render('create/');
-// });
 
-// /* POST New Create page. */
-// router.post('/new/create/', function(req, res, next) {
-//     res.render('create/');
-//     var new_ques = req.body;
-//     console.log("Ori: ");
-//     console.log(new_ques);
-//
-// });
-//
-
-
-// /* GET New Rank page. */
-// router.get('/new/rank/', controller.fetchFriendsByScore);
-//
-// /* GET New Profile page. */
-// router.get('/new/profile/:id', controller.fetchFriend);
-//
-// router.get('/new/create_mult', function(req, res, next){
-//     res.render('create/mult_choice', req.query);
-// });
-//
-// router.get('/new/create_blanks', function(req, res, next){
-//     res.render('create/blanks', req.query);
-// });
-//
-// router.get('/new/create_video', function(req, res, next){
-//     res.render('create/video', req.query);
-// });
-
-/**************************************************/
-/* Old Stuff Below */
-/* GET World page. */
-router.get('/old/world/', function(req, res, next) {
-    res.render('world');
-});
-
-/* GET FAQ page. */
-router.get('/old/faq/', function(req, res, next) {
-    res.render('faq');
-});
-
-/* GET Leaderboard page. */
-router.get('/old/leaderboard/', function(req, res, next) {
-    res.render('leaderboard');
-});
-
-/* GET Milestones page. */
-router.get('/old/milestones/', function(req, res, next) {
-    res.render('milestones');
-});
-
-/* GET Video page. */
-router.get('/old/vid/', function(req, res, next) {
-    res.render('vid');
-});
-
-/* GET Quiz page. */
-router.get('/old/quiz/', function(req, res, next) {
-    res.render('quiz');
-});
-
-/* GET Action page. */
-router.get('/old/act/', function(req, res, next) {
-    res.render('act');
+router.get('/', (req, res)=> {
+    res.redirect('/login');
 });
 
 export default router;
